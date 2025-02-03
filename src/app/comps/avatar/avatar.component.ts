c0e84f307c6a332def72a55ccdc45a97adac24a5
Stéphane Biehler <biehler@s2i-evolution.eu>
2025-02-03 14:49:07 +0100

    ✨ Improved avatar display
    Improved avatar display

--- src/app/comps/avatar/avatar.component.ts
+++ src/app/comps/avatar/avatar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { User } from '../../models/database/User';
 
 @Component({
     selector: 'app-avatar',
@@ -10,6 +11,21 @@ import { Component, Input } from '@angular/core';
 })
 export class AvatarComponent {
   @Input() name: string | null = null;
+  @Input() tooltip: string | null = null;
   @Input() avatar: string | null = null;
   @Input() avatarSize: number | null = 20;
+  @Input() user: User | null = null;
+  @Input() showName: boolean = true;
+
+  ngOnInit(){
+    if (this.user != null){
+      this.name = this.user.firstname + ' ' + this.user.lastname;
+      this.avatar = this.user.avatar;
+      this.tooltip = '<span style="font-weight: bold;">[#' + this.user.id + ']</span><br>' + this.user.firstname + ' ' + this.user.lastname + '<br>' + this.user.email + '<br><br><a class="sbi-link" href="/user/"' + this.user.id + '"><i class="fa fa-user"></i> View Profile</a>';
+    }else{
+      if (this.tooltip == null){
+        this.tooltip = this.name;
+      }
+    }
+  }
 }
